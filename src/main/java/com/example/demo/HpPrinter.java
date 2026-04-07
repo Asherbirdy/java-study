3b463548347d766e224228181db12256f20290fa
Asher Lien <birdym.tw@gmail.com>
2026-04-07 11:21:39 +0800

    sss

---
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HpPrinter implements Printer  {
+
+    @Value("${printer.name}")
+    private String name;
+
+    @Value("${printer.count:20}")
+    private int count;
+
+    @Override
+    public void print(String message) {
+        count--;
+        System.out.println(name + ':' + message);
+        System.out.println("剩餘使用次數:" + count);
+    }
+}
