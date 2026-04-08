6ae1b0e90d0e254eb3e27e57c7dd665814ceaed6
Asher Lien <birdym.tw@gmail.com>
2026-04-08 08:53:58 +0800

    asd

--- src/main/java/com/example/demo/MyAspect.java
+++ src/main/java/com/example/demo/MyAspect.java
@@ -1,21 +1,28 @@
 package com.example.demo;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Aspect
 @Component
 public class MyAspect {
 
-    @Before("execution(* com.example.demo.HpPrinter.*(..))")
-    public void before(){
-        System.out.println("I'm Before");
-    }
+    @Around("execution(* com.example.demo.HpPrinter.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
 
-    @After("execution(* com.example.demo.HpPrinter.*(..))")
-    public void after(){
-        System.out.println("I'm After");
-    }
+        Date start = new Date();
+        Object obj = pjp.proceed();
+        Date end = new Date();
+        long time = end.getTime() - start.getTime();
+        System.out.println("總共執行了:" + time + "ms");
+        return obj;
+
+   }
+
 }
